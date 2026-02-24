fccf75d18faaefdba1c1a7f311f8e7692a830dbb
nohawaheed <nohawaheed.1991@gmail.com>
2026-02-24 06:25:20 +0200

    fix: fix image path for vercel

--- src/components/sign-image/SignImage.jsx
+++ src/components/sign-image/SignImage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SignImage() {
   return (
-    <div className='hidden relative xl:flex flex-col justify-center items-center min-h-256 w-177.5 max-w-177.5 text-white bg-[url("./images/sign-image.png")] bg-cover bg-center h-screen'>
+    <div className='hidden relative xl:flex flex-col justify-center items-center min-h-256 w-177.5 max-w-177.5 text-white bg-[url("/images/sign-image.png")] bg-cover bg-center h-screen'>
       <div className='relative z-20 text-center flex flex-col gap-3.25 max-w-102.5'>
         <h1 className='text-3xl font-bold'>Chuks Kitchen</h1>
         <p className='text-xl font-medium'>Your journey to delicious, authentic Nigerian meals starts here. Sign up or log in to order your favorites today!</p>
