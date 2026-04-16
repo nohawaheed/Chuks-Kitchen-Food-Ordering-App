80434caabc83492ff4f4932092105460444f85d0
nohawaheed <nohawaheed.1991@gmail.com>
2026-04-16 20:22:19 +0200

    feat: add order flow

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function Button({title,weight = 'font-semibold',size = 'full'}) {
-    const sizeClass = {
-    sm: "btn--sm",
-    md: "btn--md",
-    lg: "btn--lg",
-    full: "btn--full"
+function Button({
+  title,
+  weight = 'font-semibold',
+  size = 'full',
+  onClick = null,
+}) {
+  const sizeClass = {
+    sm: 'btn--sm',
+    md: 'btn--md',
+    lg: 'btn--lg',
+    full: 'btn--full',
   }[size];
   return (
-    <button className={`btn ${sizeClass} hover:cursor-pointer text-white bg-primary-orange ${weight} text-md`}>{title}</button>
-  )
+    <button
+      onClick={onClick}
+      className={`btn ${sizeClass} hover:cursor-pointer text-white bg-primary-orange ${weight} text-md`}
+    >
+      {title}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
